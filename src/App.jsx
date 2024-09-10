3701fa0a84dcb4aed0e2d7ea9e03cdb47e115052
Yoshihika1414 <yoshihika117117@gmail.com>
2024-09-10 22:51:30 +0900

    Fix typo in start button

--- src/App.jsx
+++ src/App.jsx
@@ -31,7 +31,7 @@ function App() {
       <div className="mx-auto w-3/5 mt-5">
         <p className="text-center text-2xl mb-5">{(Math.floor(time / 3600)).toString().padStart(2, '0')} : {(Math.floor(time % 3600 / 60)).toString().padStart(2, '0')} : {(Math.floor(time % 60)).toString().padStart(2, '0')}</p>
         <div className="w-full md:flex md:justify-between">
-          <button onClick={handleTimerToggle} className={`text-white rounded-md p-3 w-full mb-3 md:mb-0 md:w-5/12 ${isTimerRunnig ? "bg-yellows-500" : "bg-cyan-500"}`}>
+          <button onClick={handleTimerToggle} className={`text-white rounded-md p-3 w-full mb-3 md:mb-0 md:w-5/12 ${isTimerRunnig ? "bg-yellow-500" : "bg-cyan-500"}`}>
             {isTimerRunnig ? 'Stop' : 'Start'}
           </button>
           <button onClick={resetTimer} className="bg-red-500 text-white rounded-md p-3 w-full md:w-5/12">
