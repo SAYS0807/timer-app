369c845166deae791a24f9fb3a6fedf4ceebdf23
Yoshihika1414 <yoshihika117117@gmail.com>
2024-09-14 23:00:10 +0900

    Add Time Tracker

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ function App() {
   const [isTimerRunnig, setisTimerRunnig] = useState(false);
   const [tasks, setTasks] = useState<Task[]>([]);
   const [count, setCount] = useState(0);
+  const [errorMsg, setErrorMsg] = useState<string[]>([]);
 
   useEffect(() => {
     let interval: number;
@@ -40,6 +41,20 @@ function App() {
     setTitle(input);
   }
 
+  const handleSubmit = () => {
+    let curError: string[] = [];
+
+    if (title === '') curError.push('Please fill the task name!!');
+    if (time <= 0) curError.push('You need to spend at least one seconds on this task.');
+
+    if (curError.length > 0) {
+      setErrorMsg(curError);
+      return;
+    }
+
+    submitTime();
+  }
+
   const submitTime = () => {
     const newTask: Task = {
       id: count,
@@ -63,21 +78,33 @@ function App() {
       <div className="mt-5 mx-auto md:w-3/4">
         <h1 className="text-3xl text-blue-400 underline text-center font-bold">Timer</h1>
         <div className="mx-auto w-3/5 mt-5">
-          <input type="text" onChange={(e) => handleTextChange(e.target.value)} className="w-full border-2 border-gray-600 rounded-md h-9 text-xl px-4" value={title}></input>
+          <h2 className="text-2xl">Task Name</h2>
+          <input type="text" onChange={(e) => handleTextChange(e.target.value)} className="w-full border-2 border-gray-600 rounded-md h-9 text-xl px-4" value={title} placeholder="Write the task name here..."></input>
           <p className="text-center text-2xl mb-5">{(Math.floor(time / 3600)).toString().padStart(2, '0')} : {(Math.floor(time % 3600 / 60)).toString().padStart(2, '0')} : {(Math.floor(time % 60)).toString().padStart(2, '0')}</p>
-          <div className="w-full md:flex md:justify-between">
-            <button onClick={handleTimerToggle} className={`text-white rounded-md p-3 w-full mb-3 md:mb-0 md:w-5/12 ${isTimerRunnig ? "bg-yellow-500" : "bg-cyan-500"}`}>
+          <div className="w-full md:flex md:justify-between mb-11">
+            <button onClick={handleTimerToggle} className={`text-white rounded-md p-3 w-full mb-3 md:mb-0 md:w-3/12 ${isTimerRunnig ? "bg-yellow-500" : "bg-cyan-500"}`}>
               {isTimerRunnig ? 'Stop' : 'Start'}
             </button>
-            <button onClick={resetTimer} className="bg-red-500 text-white rounded-md p-3 mb-3 w-full md:w-5/12">
+            <button onClick={resetTimer} className="bg-red-500 text-white rounded-md p-3 mb-3 w-full md:mb-0 md:w-3/12">
               Reset
             </button>
-            <button onClick={submitTime} className="bg-green-400 text-white rounded-md p-3 w-full md:w-5/12">
+          </div>
+          <div className="w-full mx-auto">
+            <p className="text-center text-xl mb-3">You've done your task?<br></br>Let's submit!!</p>
+            <button onClick={handleSubmit} className={`${time <= 0 ? "bg-gray-500" : "bg-green-400 cursor-auto"} text-white rounded-md p-3 w-full md:w-3/12`}>
               Submit
             </button>
+            <div className={`${errorMsg.length === 0 ? "hidden" : "block"} mt-3 mb-3`}>
+              <p className="text-red-500 text-center text-lg mb-1">Woops! Something is wrong.</p>
+              <ol className="ml-6 list-disc">
+                {errorMsg.map((msg, index) => (
+                  <li key={index} className="text-red-500">{msg}</li>
+                ))}
+              </ol>
+            </div>
           </div>
           <h2>Completed Tasks</h2>
-          <ul className="mt-5">
+          <ul className="mt-2">
             {tasks.map(task => (
               <li key={task.id} className="mb-3">
                 <p>{task.title}</p>
