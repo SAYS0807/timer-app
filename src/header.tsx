b940475917320680d7c8a51c76e028e57554b618
Yoshihika1414 <yoshihika117117@gmail.com>
2024-09-17 14:27:11 +0900

    Styling timer component

---
+++ src/header.tsx
@@ -0,0 +1,5 @@
+export default function Header() {
+    return (
+        <div id="header" className="header text-center text-4xl text-white font-extrabold bg-gradient-to-r from-blue-500 to-cyan-400 p-3">Timer-App</div>
+    );
+}
